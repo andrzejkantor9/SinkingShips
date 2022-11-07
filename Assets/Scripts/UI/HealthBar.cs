4e65a311a52ff3628cfec8b3bcf3b25275a74426
DESKTOP-HN11TL7\ander <andrzejkantor9@gmail.com>
2022-11-07 14:59:58 +0100

    remove empty regions

--- Assets/Scripts/UI/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar.cs
@@ -8,25 +8,12 @@ namespace SinkingShips.UI
 {
     public class HealthBar : HealthDisplay
     {
-        #region Config
-        //[Header("CONFIG")]
-        #endregion
-
         #region Cache & Constants
         [Header("CACHE")]
         [SerializeField]
         private Image _healthDisplay;
         [SerializeField]
         private Health _health;
-        #endregion
-
-        #region States
-        #endregion
-
-        #region Events & Statics
-        #endregion
-
-        #region Data
         #endregion
 
         ////////////////////////////////////////////////////////////////////////////////////////////////
@@ -58,20 +45,11 @@ namespace SinkingShips.UI
         }
         #endregion
 
-        #region Public
-        #endregion
-
         #region Interfaces & Inheritance
         protected override void UpdateDisplay(float healthPercentage)
         {
             _healthDisplay.fillAmount = healthPercentage;
         }
-        #endregion
-
-        #region Events & Statics
-        #endregion
-
-        #region Private
         #endregion
     }
 }
